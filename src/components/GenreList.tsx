6feb6a743ecdd339a91fd7ca940740dc2db8ef88
PACM-Cap <younes.alloun@capgemini.com>
2024-12-20 10:15:34 +0100

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,31 +1,56 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onSelectGenre :(genre :Genre)=> void;
-  selectedGenre : Genre | null
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre,selectedGenre} : Props) => {
-  const { data , isLoading, error} = useGenres();
-  if(error) return null;
-  if (isLoading) return <Spinner/>;
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem paddingY ='5px'key={genre.id}>
-          <HStack >
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold':'normal'} onClick={()=>onSelectGenre(genre)} fontSize='lg' variant={'link'}>{genre.name}</Button>
-          </HStack> 
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem paddingY="5px" key={genre.id}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              {/*  aspect retio of the image is desordred  */}
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant={"link"}
+              >
+                {genre.name}
+              </Button>
+              {/* button doesn't wrap the text inside it so we use whitespace to normal to do that
+            and also to align the text we use prop align  */}
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
